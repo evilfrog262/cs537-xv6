f08b9a0f9210b8efa7ae060fa17ac84b770b370c
Sid Kiblawi <skiblawi@mumble-19.cs.wisc.edu>
2013-10-21 15:27:36 -0500

    xv6

---
+++ xv6/user/fault.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+
+int
+main(int argc, char *argv[]){
+    int*p =0;
+    printf(1,"%p\n",*p);
+    //return 0;
+    exit();
+}
